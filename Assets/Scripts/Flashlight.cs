ef05b5792d0938ba4079c330737eefbbf0d82cbb
Shimychu <phenizer@hotmail.com>
2021-03-18 14:45:44 -0700

    update check on blog

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public Light light;
+    public AudioClip audioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleLight()
+    {
+        if (light.enabled)
+            light.enabled = false;
+        else
+            light.enabled = true;
+        audioSource.Play();
+    }
+
+
+}
